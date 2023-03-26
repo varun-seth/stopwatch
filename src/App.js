2f4b6919c57a8f76b3c731576eea5c0497b8cf0e
Varun Seth <8343593+varun-seth@users.noreply.github.com>
2023-03-26 21:12:05 +0530

    💄  theme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { green, lightBlue } from '@mui/material/colors';
 
 import StopWatch from './StopWatch';
 
-
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: green[300],
+            main: green[500],
+            dark: green[700],
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: lightBlue[300],
+            main: lightBlue[500],
+            dark: lightBlue[700],
+            contrastText: '#000',
+        },
+    },
+});
 
 function App() {
 
 
     return (
+        <ThemeProvider theme={theme}>
 
-        <StopWatch></StopWatch>
-
+            <StopWatch></StopWatch>
+        </ThemeProvider>
     );
 }
 
