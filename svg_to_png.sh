7114549f3bf9b7575e4ac15d0d67491749edae0a
Varun Seth <8343593+varun-seth@users.noreply.github.com>
2023-03-26 22:31:26 +0530

    💄  logo

---
+++ svg_to_png.sh
@@ -0,0 +1,3 @@
+svgexport public/logo.svg public/logo192apple.png 192:192 "svg{background:black;}"
+svgexport public/logo.svg public/logo192.png 192:192
+svgexport public/logo.svg public/logo512.png 512:512
